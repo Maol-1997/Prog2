e1a008cf0abed4596e25e2d3b5375759f8af210a
Martin Ortiz <42623358+Maol-1997@users.noreply.github.com>
2019-05-21 18:52:27 -0300

    Update HiloServidorSocket.java

--- Server/servicio/HiloServidorSocket.java
+++ Server/servicio/HiloServidorSocket.java
@@ -17,25 +17,6 @@ public class HiloServidorSocket implements Runnable {
 		try {
 			BufferedReader entrada;
 			String linea;
-			package servicio;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-
-import entidades.TarjetaCredito;
-import entidades.Venta;
-
-public class HiloServidorSocket implements Runnable {
-	private Socket socketCliente;
-	private ILogeador log;
-	private AlmacenVentas almacen;
-
-	public void run() {
-		try {
-			BufferedReader entrada;
-			String linea;
 			// --------------------------------
 			Thread.sleep(100);
 			// --------------------------------
@@ -48,29 +29,6 @@ public class HiloServidorSocket implements Runnable {
 				almacen.push(venta);
 			}
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	public HiloServidorSocket(Socket socketCliente, ILogeador log, AlmacenVentas almacen) {
-		this.socketCliente = socketCliente;
-		this.log = log;
-		this.almacen = almacen;
-	}
-}
-
-				while ((linea = entrada.readLine()) != null) {
-					log.info("Recibido de internet " + linea);
-					String[] datos = linea.split(",");
-					TarjetaCredito tarjeta = new TarjetaCredito(datos[0], datos[1], datos[2], datos[3]);
-					Venta venta = new Venta(datos[4], Float.valueOf(datos[5]), tarjeta, socketCliente);
-					almacen.push(venta);
-				}
-			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
